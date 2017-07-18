652a9cd02168c61bf5f74eca00cd9a0360aa0c59
mikecrinite <mikecrinite@gmail.com>
2017-07-17 20:03:04 -0400

    Messing with font settings

--- app/src/main/java/com/crinite/mike/colorpalette/activities/ColorsViewActivity.java
+++ app/src/main/java/com/crinite/mike/colorpalette/activities/ColorsViewActivity.java
@@ -1,15 +1,18 @@
 package com.crinite.mike.colorpalette.activities;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.crinite.mike.colorpalette.R;
 import com.crinite.mike.colorpalette.objects.ColorAssociation;
 import com.crinite.mike.colorpalette.services.ColorDictionaryService;
+import com.crinite.mike.colorpalette.services.ColorService;
 
 import java.util.ArrayList;
 
@@ -17,21 +20,23 @@ import java.util.ArrayList;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class ColorsViewActivity extends AppCompatActivity {
+public class ColorsViewActivity extends AppCompatActivity implements View.OnClickListener {
     //Views
     FrameLayout content;
 
     //Fields
+    boolean background = true;
 
      @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_colors_view);
+         setContentView(R.layout.activity_colors_view);
 
-        content = (FrameLayout) findViewById(R.id.content);
+         content = (FrameLayout) findViewById(R.id.content);
+         content.setOnClickListener(this);
 
-        content.addView(dynamicList());
+         content.addView(dynamicList());
     }
     private LinearLayout dynamicList(){
         LinearLayout outer = new LinearLayout(getApplicationContext());
@@ -44,7 +49,7 @@ public class ColorsViewActivity extends AppCompatActivity {
         String name;
         int color;
         TextView txtName;
-        ImageView imgCol;
+        //View viewCol;
 
         for(ColorAssociation ca : colorList){
             inner = generateNonDefaultLinearLayout();
@@ -52,13 +57,16 @@ public class ColorsViewActivity extends AppCompatActivity {
             color = ca.getColor();
 
             txtName = new TextView(getApplicationContext());
-            imgCol = new ImageView(getApplicationContext());
+            //viewCol = new ImageView(getApplicationContext());
 
             txtName.setText(name);
-            imgCol.setBackgroundColor(color);
+            txtName.setTextColor(color);
+            txtName.setTextSize(30);
+            txtName.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+            //viewCol.setBackgroundColor(color);
 
             inner.addView(txtName);
-            inner.addView(imgCol);
+            //inner.addView(viewCol);
 
             outer.addView(inner);
         }
@@ -69,9 +77,28 @@ public class ColorsViewActivity extends AppCompatActivity {
     private LinearLayout generateNonDefaultLinearLayout(){
         LinearLayout result = new LinearLayout(getApplicationContext());
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.gravity = Gravity.CENTER;
         result.setLayoutParams(params);
         result.setOrientation(LinearLayout.HORIZONTAL);
 
         return result;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.content:
+                swapBackgroundColor();
+                break;
+        }
+    }
+
+    private void swapBackgroundColor() {
+        if(background) {
+            content.setBackgroundColor(ColorService.getInstance().colorFromHex("#FFFFFF"));
+        }else{
+            content.setBackgroundColor(ColorService.getInstance().colorFromHex("#000000"));
+        }
+        background = !background;
     }
 }
